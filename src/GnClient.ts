8bc0eeda400cbb6f20aaf3cf3e5a7e4612d3876b
pariamahmoudi <paria.mahmoudi@gmail.com>
2021-05-01 14:33:23 +0430

    harchi

--- src/GnClient.ts
+++ src/GnClient.ts
@@ -1,9 +1,21 @@
-
+//const signalR = require('@microsoft/signalr')
+import { HubConnectionBuilder, HttpTransportType, LogLevel } from '@microsoft/signalr'
+let connection = new HubConnectionBuilder()
+  .configureLogging(LogLevel.Trace)
+  .withUrl("http://172.16.6.158/:80/chathub", {
+    skipNegotiation: false,
+    transport: HttpTransportType.WebSockets
+  })
+  .build();
 class GnClient {
-    public connect(url: string): string {
-      console.log(url);
-      return url;
-    }
+  public connect(url: string): string {
+    connection.start()
+      .then(() => connection.invoke("send", "Hello"))
+      .catch(err => {
+        console.log(err)
+      })
+    return url;
   }
-  
-  export {GnClient};
+}
+
+export { GnClient };
